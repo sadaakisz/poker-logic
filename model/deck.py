068aff08f59be3c06bb955ca04730279829dcc85
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-02-06 10:27:31 -0500

    Added return types to functions.

--- model/deck.py
+++ model/deck.py
@@ -6,15 +6,15 @@ class Deck:
         self.cards = []
         self.create_deck()
     
-    def create_deck(self):
+    def create_deck(self) -> None:
         self.cards = []
         for symbol in list(crd.Symbol):
             for value in list(crd.Value):
                 self.cards.append(crd.Card(value.value, symbol.value))
         self.shuffle_deck()
     
-    def shuffle_deck(self):
+    def shuffle_deck(self) -> None:
         random.shuffle(self.cards)
 
-    def get_card(self):
+    def get_card(self) -> crd.Card:
         return self.cards.pop()
