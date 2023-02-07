fef88b71bef92f388b438ad96f4b66e5971a316e
Hector Suzuki <40412908+sadaakisz@users.noreply.github.com>
2023-02-07 14:59:47 +0000

    Implemented Two Pair and Pair detection

--- logic/hand_ranker.py
+++ logic/hand_ranker.py
@@ -1,25 +1,28 @@
 from copy import deepcopy
 
 def check_special_flush(dict_hand: dict, max_value: int) -> bool:
-    if max_value not in dict_hand.keys():
+    fl_dict_hand = deepcopy(dict_hand)
+    if max_value not in fl_dict_hand.keys():
         return False
-    if 14 in dict_hand.keys():
-        dict_hand[1] = dict_hand[14]
-    value_index = list(dict_hand.keys()).index(max_value)
-    possible_sf = sum(list(dict_hand.keys())[value_index:value_index+5])==(max_value-2)*5 
+    if 14 in fl_dict_hand.keys():
+        fl_dict_hand[1] = fl_dict_hand[14]
+    value_index = list(fl_dict_hand.keys()).index(max_value)
+    possible_sf = sum(list(fl_dict_hand.keys())[value_index:value_index+5])==(max_value-2)*5 
     if possible_sf:
         max_symbol = ''
-        for symbol in dict_hand[max_value]:
+        for symbol in fl_dict_hand[max_value]:
             max_symbol = symbol
             other_symbols = []
             for i in range(max_value-1, max_value-5, -1):
-                other_symbols.append(dict_hand[i])
+                other_symbols.append(fl_dict_hand[i])
             if other_symbols == [[max_symbol] for _ in range(4)]:
                 return True
     return False
 
-def check_pair(dict_hand: dict) -> str:
+def check_pair(dict_hand: dict, existing_pair=0) -> str:
     for key in dict_hand:
+        if key==existing_pair:
+            continue
         if len(dict_hand[key])==2:
             return str(key)+' Pair'
     return ''
@@ -32,6 +35,10 @@ def check_repeated(dict_hand: dict) -> str:
             pair = check_pair(dict_hand)
             if pair: return str(key)+' '+pair.split()[0]+' Full House'
             return str(key)+' Three of a Kind'
+        if len(dict_hand[key])==2:
+            pair = check_pair(dict_hand, key)
+            if pair: return str(key)+' '+pair.split()[0]+' Two Pairs'
+            return str(key)+' Pair'
     return ''
 
 def rank(dict_hand: dict) -> str:
@@ -47,5 +54,5 @@ def rank(dict_hand: dict) -> str:
     repeated = check_repeated(dict_hand)
     if repeated: return repeated
 
-    return 'High Card'
+    return str(list(dict_hand.keys())[0])+' High Card'
 
