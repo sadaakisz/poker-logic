c28468b6bc265a71eab89083d5ae4d249a38da96
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-02-06 20:18:59 -0500

    Implemented dict_hand to return hand as a dict.

--- model/pocket.py
+++ model/pocket.py
@@ -10,5 +10,14 @@ class Pocket:
     def hand(self, board) -> list:
         return sorted(self.cards+board, reverse=True)
     
+    def dict_hand(self, board) -> dict:
+        hand_dict = {}
+        hand_tuples = [card.as_tuple() for card in self.hand(board)]
+        for card_tuple in hand_tuples:
+            if hand_dict.get(card_tuple[0]) is None:
+                hand_dict[card_tuple[0]] = []
+            hand_dict[card_tuple[0]].append(card_tuple[1])
+        return hand_dict
+    
     def __repr__(self) -> str:
         return str(self.cards)
