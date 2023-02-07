5e8927728f941bb0d9c8071e5c5a15debae09c64
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-02-06 20:18:11 -0500

    Freestyling logic.

--- logic/hand_ranker.py
+++ logic/hand_ranker.py
@@ -1,9 +1,16 @@
 import model.card as crd
-def rank(hand: list) -> str:
-    tuples = [card.as_tuple() for card in hand]
-
-    print(tuples)
+def rank(dict_hand: dict) -> str:
+    print(dict_hand)
+    
+    return
 
     # Royal Flush
-    if len(hand)>=5:
-        pass
+    possible_rf = sum(dict_hand.keys()[:5])==60 # Figure out the formula
+    if possible_rf:
+        flush_symbol = ''
+        for i in range(14, 9, -1):
+            for symbol in dict_hand[i]:
+                flush_symbol = symbol
+        for symbol in dict_hand(14):
+            # Check if symbol is present in next 5
+
