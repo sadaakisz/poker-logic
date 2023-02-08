2668d0ef22be193b35c231bcca20d3e595deddbf
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-02-07 20:14:05 -0500

    Added hand class dictionary.

---
+++ tester/test_runner.py
@@ -0,0 +1,13 @@
+hand_eq = {
+    'Royal_Flush': 9,
+    'Straight_Flush': 8,
+    'Four_of_a_Kind': 7,
+    'Full_House': 6,
+    'Flush': 5,
+    'Straight': 4,
+    'Three_of_a_Kind': 3,
+    'Two_Pairs': 2,
+    'Pair': 1,
+    'High_Card': 0
+}
+
