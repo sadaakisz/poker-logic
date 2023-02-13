838a24439545572a0f98ab689e16c4f806e52aa6
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2023-02-12 22:22:42 -0500

    Testing early table logic.

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ import logic.hand_ranker as hr
 import logic.hand_maker as hm
 import dataset.test_runner as tr
 
-tr.run_test()
-
 for a in range(1):
-    break
     deck = dck.Deck()
     board = []
-    pocket = pck.Pocket()
+    p1 = pck.Pocket()
+    p2 = pck.Pocket()
+    p3 = pck.Pocket()
+    p4 = pck.Pocket()
 
-    # Royal Flush
-    rf_hand = hm.make_hand(['14H', '13H', '12H', '11H', '10H', '9H', '8H'])
-    # Straight Flush (edge)
-    sf_hand1 = hm.make_hand(['14C', '13H', '12H', '11H', '10H', '9H', '8H'])
-    # Straight Flush
-    sf_hand2 = hm.make_hand(['9C', '8C', '7C', '6C', '5C', '9H', '8H'])
-    # Straight Flush (edge)
-    sf_hand3 = hm.make_hand(['5S', '4S', '3S', '2S', '14S', '9H', '8H'])
-    print(hr.rank(rf_hand))
-    print(hr.rank(sf_hand1))
-    print(hr.rank(sf_hand2))
-    print(hr.rank(sf_hand3))
+    for i in range(2):
+        p1.append(deck.get_card())
+        p2.append(deck.get_card())
+        p3.append(deck.get_card())
+        p4.append(deck.get_card())
+    
+    for i in range(5):
+        board.append(deck.get_card())
+
+    print(p1)
+    print(p2)
+    print(p3)
+    print(p4)
+    print(board)
+
+    list_ranks = []
+    list_ranks.append(hr.list_rank(p1.dict_hand(board)))
+    list_ranks.append(hr.list_rank(p2.dict_hand(board)))
+    list_ranks.append(hr.list_rank(p3.dict_hand(board)))
+    list_ranks.append(hr.list_rank(p4.dict_hand(board)))
+
+    print(hr.winner(list_ranks, show_hand=True))
